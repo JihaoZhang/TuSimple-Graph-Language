7e647e8e64ad763ee6a922634d7ad8995799fdb6
JihaoZhang <jihaozhang001@gmail.com>
2017-04-22 17:58:12 -0400

    Tusimple V0.0.1

---
+++ _build/parser.mli
@@ -0,0 +1,38 @@
+type token =
+  | SEMI
+  | LPAREN
+  | RPAREN
+  | LBRACE
+  | RBRACE
+  | COMMA
+  | PLUS
+  | MINUS
+  | TIMES
+  | DIVIDE
+  | ASSIGN
+  | NOT
+  | ADDASSIGN
+  | EQ
+  | NEQ
+  | LT
+  | LEQ
+  | GT
+  | GEQ
+  | TRUE
+  | FALSE
+  | AND
+  | OR
+  | RETURN
+  | IF
+  | ELSE
+  | FOR
+  | WHILE
+  | INT
+  | BOOL
+  | VOID
+  | LITERAL of (int)
+  | ID of (string)
+  | EOF
+
+val program :
+  (Lexing.lexbuf  -> token) -> Lexing.lexbuf -> Ast.program
